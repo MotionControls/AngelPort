de7c1c2d375cfdee5f7484026ca542c5f526fc31
MotionControls <bprogers43@tntech.edu>
2025-02-02 11:45:06 -0600

    Added two backgrounds.

--- textures.h
+++ textures.h
@@ -7,11 +7,15 @@ extern unsigned long _binary_res_objs1_tim_start[];
 extern unsigned long _binary_res_objs2_tim_start[];
 extern unsigned long _binary_res_objs3_tim_start[];
 extern unsigned long _binary_res_objs4_tim_start[];
+extern unsigned long _binary_res_bg1_tim_start[];
+extern unsigned long _binary_res_bg2_tim_start[];
 
 TIM_IMAGE timObjs1;
 TIM_IMAGE timObjs2;
 TIM_IMAGE timObjs3;
 TIM_IMAGE timObjs4;
+TIM_IMAGE timBG1;
+TIM_IMAGE timBG2;
 
 void LoadTexture(u_long* tim, TIM_IMAGE* tparam){	// Credit: Lameguy64 ; lameguy64.net/svn/pstutorials/chapter1/3-textures.html
 	OpenTIM(tim);		// Opens the TIM binary. Unsure what that means.
@@ -31,6 +35,9 @@ void InitTextures(){
 	LoadTexture(_binary_res_objs2_tim_start, timObjs2);
 	LoadTexture(_binary_res_objs3_tim_start, timObjs3);
 	LoadTexture(_binary_res_objs4_tim_start, timObjs4);
+	
+	LoadTexture(_binary_res_objs4_tim_start, timBG1);
+	LoadTexture(_binary_res_objs4_tim_start, timBG2);
 }
 
 #endif
